a8e7a246a5e3e16f0e628a22274dfe8082fdecb7
joeywangkds <spiderkds@gmail.com>
2023-11-30 09:38:16 +1100

    1130

--- back-end/6.1 Blog API Project/index.js
+++ back-end/6.1 Blog API Project/index.js
@@ -41,14 +41,69 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
-
+app.get("/posts", (req, res) => {
+  // console.log(posts);
+  res.json(posts);
+});
 //CHALLENGE 2: GET a specific post by id
+
+app.get("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const postById = posts.find((post) => post.id == id);
+  res.json(postById);
+});
 
 //CHALLENGE 3: POST a new post
 
+app.post("/posts", (req, res) => {
+  // let localTimeString = dateObject.toLocaleString();
+  // console.log("本地时间：" + localTimeString);
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+
+  posts.push(newPost);
+  res.json(newPost);
+});
+
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
+app.patch("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPost = posts.find((post) => (post.id = id));
+
+  const newPost = {
+    id: id,
+    title: req.body.title || currentPost.title,
+    content: req.body.content || currentPost.content,
+    author: req.body.author || currentPost.author,
+    date: new Date(),
+  };
+
+  const postIndex = posts.findIndex((post)=>post.id = id);
+  posts[postIndex] = newPost;
+  res.json(newPost);
+});
+
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+
+app.delete("/posts/:id", (req,res)=>{
+  const id = parseInt(req.params.id);
+  const postIndex = posts.findIndex((post)=>post.id = id);
+  if(postIndex > -1){
+    posts.splice(postIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Post with id: ${id} not found. No post were deleted.` });
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
